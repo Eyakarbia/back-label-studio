d70d2d967502087ffaa147dd05155293d6be025f
Ameny Karbia <amenykarbia@gmail.com>
2024-08-06 09:51:28 +0100

    register

--- app/modules/user/model.js
+++ app/modules/user/model.js
@@ -7,6 +7,8 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, required: true },
+    phone:{  type: String, required: true },
+    addresse:{ type: String, required: true }
 });
 
 // Method to compare passwords
